36396fdcda0192fc575d7b8fd9b7cd268e0fbb0d
elmar <elmar@mediamodifier.com>
2022-07-20 17:31:04 +0300

    clean

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ use std::path::Path;
 
 pub mod graph {
     use crate::file_analyzer::Node;
-    use std::{fmt::Write, io::Error, ffi::OsString};
+    use std::fmt::Write;
 
     pub struct Graph {
         elements: Vec<Node>,
@@ -44,41 +44,42 @@ pub mod graph {
         /// Find the largest file and return its size, name and parent folder
         pub fn largest_file(&self) -> (String, String, u64) {
             let mut s = 0u64;
-            let mut st = String::new();
+            // let mut st = String::new();
             let mut index: Option<usize> = None;
             for f in self.elements.iter().enumerate() {
                 if f.1.size > s {
                     s = f.1.size;
-                    st = f.1.name.clone();
                     index = Some(f.0);
                 }
             }
-            // let it = self.relations.iter().cycle();
-            // let node_path = Vec::new();
-            let mut dir_path: Vec<OsString> = Vec::new();
+            let n = self.elements.get(index.unwrap()).unwrap();
+            let s = n.size;
+            let st = n.name.clone();
+            let mut dir_path: Vec<String> = Vec::new();
             loop {
-                let relation = self.relations.iter().find(|f| f.children.contains(&index.unwrap()));
+                let relation = self
+                    .relations
+                    .iter()
+                    .find(|f| f.children.contains(&index.unwrap()));
                 match relation {
                     Some(r) => {
-                        let parent_folder = self.elements.get(r.parent).unwrap().path.clone();
-                        dir_path.push(parent_folder.clone());
+                        let parent_folder = self.elements.get(r.parent).unwrap().name.clone();
+                        dir_path.push(parent_folder);
                         index = Some(r.parent);
-                    },
-                    None => break
-                }    
-            };
-            // let res = self.relations.iter().find(|f| f.children.contains(&index.unwrap())).unwrap();
-            // let parent_index = res.parent;
-            // let parent_folder = self.elements.get(parent_index).unwrap().name.clone();
-
-            // with OsString
-            let mut fstr: Vec<String> = Vec::new();
-            for f in dir_path {
-                let h = f.to_str().map(|s| s.to_string()).unwrap();
-                fstr.push(h);
+                    }
+                    None => break,
+                }
             }
-            
-            (fstr.join("/"), st, s)
+            let dir_path: String = dir_path
+                .iter()
+                .rev()
+                .map(|s| {
+                    let mut s = s.clone();
+                    s.push('/');
+                    return s;
+                })
+                .collect();
+            (dir_path, st, s)
         }
 
         pub fn push(&mut self, n: Node, i: Option<usize>) -> usize {
@@ -119,12 +120,12 @@ pub mod file_analyzer {
 
     /// File descriptior
     pub struct Node {
-        pub name: String,         // name of file
+        pub name: String,             // name of file
         pub path: std::ffi::OsString, // path to file (platform independent)
-        pub size: u64,            // size in bytes of file
-        system: bool,             // system file or similar, not safe to delete
-        category: Category,       // category of file
-        is_dir: bool,             // is current node a dir
+        pub size: u64,                // size in bytes of file
+        system: bool,                 // system file or similar, not safe to delete
+        category: Category,           // category of file
+        is_dir: bool,                 // is current node a dir
     }
 
     impl Default for Node {
@@ -140,82 +141,21 @@ pub mod file_analyzer {
         }
     }
 
-    pub struct NodeList {
-        root: Node,
-        root_path: OsString,
-    }
-
-    trait Crud {
-        fn add_child(&mut self, node: Node);
-    }
-
-    // impl Crud for Node {
-    //     fn add_child(&mut self, node: Node) {
-    //         self.children.push(node);
-    //     }
-    // }
-
     pub fn start(root: OsString) -> Result<(), Error> {
         let mut db = Graph::new();
-        let s = root.to_os_string();
         let node = Node {
             path: OsString::from(&root),
             is_dir: true,
             ..Default::default()
         };
-        // let mut nlist = NodeList {
-        //     root_path: s,
-        //     root: node,
-        // };
         db.push(node, None);
-        // let node = &mut nlist.root;
         get_nodes(Path::new(&root), 0, &mut db)?;
         println!(
             "Length of data and relations: {}, {}",
             db.elements_len(),
             db.relations_len()
         );
-        // db.print_rel(5).unwrap();
         println!("{:#?}", db.largest_file());
-        Ok(())
-    }
-
-    // pub fn print_list(nl: &NodeList) {
-    //     let r = &nl.root;
-    //     for c in &r.children {
-    //         match c.is_dir {
-    //             true => {
-    //                 println!("dir: {}", c.name);
-    //                 println!("-----");
-    //             }
-    //             false => println!("{}", c.name),
-    //         }
-    //         println!("{}", c.size);
-    //     }
-    //     for c in &r.children {
-    //         print_nodes(c);
-    //     }
-    // }
-
-    // pub fn print_nodes(n: &Node) {
-    //     match n.is_dir {
-    //         true => {
-    //             println!("dir: {}", n.name);
-    //             println!("-----");
-    //         }
-    //         false => println!("{}", n.name),
-    //     }
-    //     for c in &n.children {
-    //         print_nodes(c);
-    //     }
-    // }
-
-    pub fn read_dir(path: &Path) -> Result<(), Error> {
-        for entry in fs::read_dir(path)? {
-            let entry = entry?;
-            let path = entry.path();
-            println!("{}", path.to_str().unwrap());
-        }
         Ok(())
     }
 
@@ -227,14 +167,13 @@ pub mod file_analyzer {
         return Ok(Node {
             name,
             size: size,
-            path: OsString::from(&path),
+            // path: OsString::from(&path),
             is_dir,
             ..Default::default()
         });
     }
 
     pub fn get_nodes(dir: &Path, parent_index: usize, db: &mut Graph) -> io::Result<()> {
-        // std::mem::size_of::Node;
         if dir.is_dir() {
             for entry in fs::read_dir(dir)? {
                 let entry = entry?;
@@ -248,30 +187,6 @@ pub mod file_analyzer {
                     get_nodes(&path, parent_index, db)?;
                 }
             }
-        }
-        Ok(())
-    }
-
-    pub fn visit_dirs(dir: &Path, cb: &dyn Fn(&DirEntry)) -> io::Result<()> {
-        // std::mem::size_of::Node;
-        let s = std::mem::size_of::<Node>();
-        println!("Size is: {s}");
-        if dir.is_dir() {
-            let mut x = 0;
-            for entry in fs::read_dir(dir)? {
-                let entry = entry?;
-
-                let path = entry.path();
-                if path.is_dir() {
-                    visit_dirs(&path, cb)?;
-                } else {
-                    let size = path.metadata().unwrap().len();
-                    println!("{size}");
-                    x += 1;
-                    cb(&entry);
-                }
-            }
-            println!("{x}");
         }
         Ok(())
     }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,6 @@
 use fs_analyzer::file_analyzer;
-use std::{path::Path, fs::DirEntry, ffi::OsString};
+use std::{path::Path, ffi::OsString};
 use rustop::opts;
-
-fn print_name(entry: &DirEntry) {
-    println!("{}", entry.file_name().to_str().unwrap())
-}
 
 fn main() {
     let (args, _rest) = opts! {
@@ -12,13 +8,6 @@ fn main() {
         opt verbose:bool, desc:"Be verbose.";               // a flag -v or --verbose
         param dir:String, desc:"Input file name."; 
     }.parse_or_exit();
-    // let s = ".";
     let fixed_path = Path::new(&args.dir);
-    // file_analyzer::read_dir(&fixed_path).unwrap();    
-    // file_analyzer::visit_dirs(&fixed_path, &print_name).unwrap();    
-
-    // file_analyzer::start(OsString::from(&fixed_path)).unwrap();
     file_analyzer::start(OsString::from(&fixed_path)).unwrap();
-
-    println!("Hello, world!");
 }
